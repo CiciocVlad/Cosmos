def215757bd493a5ef2bded7c5b63d0fc5dea273
Cicioc Vlad <ciciocvlad@gmail.com>
2023-12-09 11:35:22 +0200

    init cosmos

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, useGLTF } from '@react-three/drei'
+
+export const App = () => {
+  const cosmos = useGLTF('./cosmos/BB-8-1.glb')
+
+  return (
+    <div className="flex justify-center items-center h-screen w-screen">
+      <Canvas className="cursor-pointer" frameloop="demand" camera={{ position: [-4, 3, 6], fov: 45, near: 0.1, far: 200 }}>
+        <OrbitControls autoRotate enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} enablePan={false} />
+        <primitive object={cosmos.scene} scale={2.5} />
+      </Canvas>
+    </div>
+  )
+}
