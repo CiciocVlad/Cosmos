ccacd7eb927996676555ccdc6d38af4ba1a34a93
Cicioc Vlad <ciciocvlad@gmail.com>
2023-12-09 12:18:55 +0200

    made cosmos bigger

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls, useGLTF } from '@react-three/drei'
 
@@ -5,10 +6,10 @@ export const App = () => {
   const cosmos = useGLTF('./cosmos/BB-8-1.glb')
 
   return (
-    <div className="flex justify-center items-center h-screen w-screen">
-      <Canvas className="cursor-pointer" frameloop="demand" camera={{ position: [-4, 3, 6], fov: 45, near: 0.1, far: 200 }}>
+    <div className="wrapper">
+      <Canvas className="robot" frameloop="demand" camera={{ position: [-4, 3, 6], fov: 45, near: 0.1, far: 500 }}>
         <OrbitControls autoRotate enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} enablePan={false} />
-        <primitive object={cosmos.scene} scale={2.5} />
+        <primitive object={cosmos.scene} scale={0.5} />
       </Canvas>
     </div>
   )
